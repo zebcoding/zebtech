a2381368d36d56b28dfaf1c2ac863d69f617177e
MckenzieSkullKid <mckenzie_callum@live.com>
2014-05-16 22:22:10 +0100

    Just need to edit the GUI a little and also add textures

--- src/main/java/uk/co/zebcoding/zebtech/network/PacketHandler.java
+++ src/main/java/uk/co/zebcoding/zebtech/network/PacketHandler.java
@@ -5,10 +5,7 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
 import uk.co.zebcoding.zebtech.help.Reference;
-import uk.co.zebcoding.zebtech.network.message.MessageTileEntityPipe;
-import uk.co.zebcoding.zebtech.network.message.MessageTileEntityTank;
-import uk.co.zebcoding.zebtech.network.message.MessageTileEntityZechoriumExciter;
-import uk.co.zebcoding.zebtech.network.message.MessageTileEntityZechoriumInfuser;
+import uk.co.zebcoding.zebtech.network.message.*;
 
 public class PacketHandler {
     public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel(Reference.MODID.toLowerCase());
@@ -18,5 +15,6 @@ public class PacketHandler {
         INSTANCE.registerMessage(MessageTileEntityPipe.class, MessageTileEntityPipe.class, 1, Side.CLIENT);
         INSTANCE.registerMessage(MessageTileEntityZechoriumInfuser.class, MessageTileEntityZechoriumInfuser.class, 2, Side.CLIENT);
         INSTANCE.registerMessage(MessageTileEntityTank.class, MessageTileEntityTank.class, 3, Side.CLIENT);
+        INSTANCE.registerMessage(MessageTileEntityZechoriumCompressor.class, MessageTileEntityZechoriumCompressor.class, 3, Side.CLIENT);
     }
 }
